493506606b1689792c636bf051d30f2b5e4e0cc3
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2024-08-28 22:49:56 +0200

    adds todos motion keymaps

--- lua/kadza/plugins/todo_comments.lua
+++ lua/kadza/plugins/todo_comments.lua
@@ -2,6 +2,22 @@ return {
   "folke/todo-comments.nvim",
   event = { "BufReadPre", "BufNewFile" },
   dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    {
+      "]t",
+      function()
+        require("todo-comments").jump_next()
+      end,
+      desc = "Next Todo Comment",
+    },
+    {
+      "[t",
+      function()
+        require("todo-comments").jump_prev()
+      end,
+      desc = "Previous Todo Comment",
+    },
+  },
   config = function()
     local todo_comments = require("todo-comments")
 
