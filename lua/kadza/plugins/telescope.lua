871daaf96fb91c6313f499ac2fddf7b0902e6c0e
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2024-08-28 23:10:06 +0200

    removes <leader>: keymap as history is available in cmdline

--- lua/kadza/plugins/telescope.lua
+++ lua/kadza/plugins/telescope.lua
@@ -36,7 +36,6 @@ return {
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-    keymap.set("n", "<leader>:", "<cmd>Telescope command_history<cr>", { desc = "Command History" })
     keymap.set("n", ":", "<cmd>Telescope cmdline<cr>", { desc = "Cmdline" })
   end,
 }
