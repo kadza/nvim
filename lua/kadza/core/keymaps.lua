64ac07b90eb143ba0f7fb7b0de9fbd6fc6688c0a
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2024-09-03 00:02:28 +0200

    split window keymaps

--- lua/kadza/core/keymaps.lua
+++ lua/kadza/core/keymaps.lua
@@ -12,7 +12,7 @@ keymap.set("i", "<C-c>", "<Esc>", { desc = "Esc" })
 keymap.set("v", "<C-c>", "<Esc>", { desc = "Esc" })
 keymap.set("i", "jk", "<Esc>", { desc = "Esc" })
 keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Rename under cursor" })
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
