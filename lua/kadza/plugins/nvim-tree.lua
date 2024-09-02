69d2cae6935b8b90fc1ba60b48a24e660aa13182
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2024-09-02 15:32:09 +0200

    reconfigures nvim-tree to suit my needs

--- lua/kadza/plugins/nvim-tree.lua
+++ lua/kadza/plugins/nvim-tree.lua
@@ -10,7 +10,7 @@ return {
 
     nvimtree.setup({
       view = {
-        width = 35,
+        width = 45,
         relativenumber = true,
       },
       -- change folder arrow icons
@@ -49,8 +49,9 @@ return {
     local keymap = vim.keymap -- for conciseness
 
     keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+    vim.keymap.set("n", "<leader>ef", function()
+      require("nvim-tree.api").tree.find_file({ open = true, focus = true })
+    end, { noremap = true, silent = true })
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-  end
+  end,
 }
