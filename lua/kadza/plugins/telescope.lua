91abaa0b3be91a3091b0a255c33c55bf3bcc0862
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2024-09-04 01:18:55 +0200

    Revert "adds telescope-cmdline"
    This reverts commit 9f3bd9b0006e7db0f992ebaf7ec9f71bb3ac6d36.

--- lua/kadza/plugins/telescope.lua
+++ lua/kadza/plugins/telescope.lua
@@ -6,7 +6,6 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
     "folke/todo-comments.nvim",
-    "jonarrien/telescope-cmdline.nvim",
   },
   config = function()
     local telescope = require("telescope")
@@ -36,6 +35,5 @@ return {
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-    keymap.set("n", ":", "<cmd>Telescope cmdline<cr>", { desc = "Cmdline" })
   end,
 }
