f69d22fd5f251d04a3e0cf7515f7229bb84d2d7a
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2024-10-02 14:57:38 +0200

    dadbod - disables foldenable

--- lua/kadza/plugins/vim-dadbod.lua
+++ lua/kadza/plugins/vim-dadbod.lua
@@ -1,4 +1,12 @@
 return {
   "tpope/vim-dadbod",
   cmd = "DB",
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "dbout" },
+      callback = function()
+        vim.opt.foldenable = false
+      end,
+    })
+  end,
 }
