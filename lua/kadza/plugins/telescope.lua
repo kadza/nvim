d92c465260252f1ada92ff6a66adea7a66e564d7
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2024-12-06 21:43:52 +0100

    removes comments from telescope plugin conf

--- lua/kadza/plugins/telescope.lua
+++ lua/kadza/plugins/telescope.lua
@@ -17,31 +17,28 @@ return {
         path_display = { "smart" },
         mappings = {
           i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
             ["<C-h>"] = require("telescope.actions").cycle_history_prev,
           },
         },
       },
     })
 
-    -- Custom live_grep wrapper
     local function live_grep_min_length()
       local input = vim.fn.input("Grep: ")
       builtin.live_grep({
-        default_text = input, -- Pre-fill the search query
+        default_text = input,
       })
     end
 
     telescope.load_extension("fzf")
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+    local keymap = vim.keymap
 
     keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader><leader>", "<cmd>Telescope git_files<cr>", { desc = "Fuzzy find files in cwd" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    -- keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fs", live_grep_min_length, { noremap = true, silent = true })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
