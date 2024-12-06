8d67e6483a0f2a0c1051f0c7fb6f8f9441ddfdc7
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2024-12-06 21:58:53 +0100

    adds clear search highlights keymap

--- lua/kadza/core/keymaps.lua
+++ lua/kadza/core/keymaps.lua
@@ -8,11 +8,10 @@ keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page jump up with cursor in t
 keymap.set("n", "n", "nzzzv", { desc = "Next search term with cursor in the middle" })
 keymap.set("n", "N", "Nzzzv", { desc = "Previous search term with cursor in the middle" })
 keymap.set("x", "<leader>p", [["_dP]], { desc = "keeps the pasted value in a buffer instead of replacing it" })
-keymap.set("i", "<C-c>", "<Esc>", { desc = "Esc" })
-keymap.set("v", "<C-c>", "<Esc>", { desc = "Esc" })
 keymap.set("i", "jk", "<Esc>", { desc = "Esc" })
 keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Rename under cursor" })
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>nc", ":nohl<CR>", { desc = "Clear search highlights" })
