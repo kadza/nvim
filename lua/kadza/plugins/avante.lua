4727a6844ff3899015cf1d31ad39b6cbf7db6995
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2025-06-07 22:05:32 +0200

    avante configuration changes

--- lua/kadza/plugins/avante.lua
+++ lua/kadza/plugins/avante.lua
@@ -9,11 +9,15 @@ return {
     provider = "openai",
     openai = {
       endpoint = "https://api.openai.com/v1",
-      model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+      model = "gpt-4.1-mini", -- your desired model (or use gpt-4o, etc.)
+      -- model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
       timeout = 30000, -- timeout in milliseconds
       temperature = 0, -- adjust if needed
       max_tokens = 4096,
-      -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+      reasoning_effort = "low", -- only supported for reasoning models (o1, etc.)
+    },
+    behaviour = {
+      use_cwd_as_project_root = true,
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
